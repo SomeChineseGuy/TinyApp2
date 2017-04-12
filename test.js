963c200be272e0460f851f994af2fb418bfa0d4a
vagrant <alvin.cl.ng@gmail.com>
2017-04-12 00:04:57 +0000

    First Part Done

---
+++ test.js
@@ -0,0 +1,13 @@
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+for(i in urlDatabase) {
+  console.log(urlDatabase[i].key)
+}
+//  console.log(urlDatabase.key)
+
+
+// console.log(urlDatabase)
+
